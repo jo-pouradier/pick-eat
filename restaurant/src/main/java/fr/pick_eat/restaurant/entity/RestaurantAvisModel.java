27c8c4abd0a0bbc87255e46474c7ffe38934716c
Adri1 <adrien.dalbeigue@cpe.fr>
2025-01-08 17:24:57 +0100

    first commit restaurantService

---
+++ restaurant/src/main/java/fr/pick_eat/restaurant/entity/RestaurantAvisModel.java
@@ -0,0 +1,36 @@
+package fr.pick_eat.restaurant.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.Generated;
+
+import java.util.UUID;
+
+@Entity
+public class RestaurantAvisModel {
+    @Id
+    @Generated
+    private UUID id;
+    private UUID restaurantId;
+    private String comment;
+
+    public RestaurantAvisModel() {}
+
+    public RestaurantAvisModel(UUID restaurantId, String comment) {
+        this.id = UUID.randomUUID();
+        this.restaurantId = restaurantId;
+        this.comment = comment;
+    }
+
+    public UUID getId() {return id;}
+
+    public void setId(UUID id) {this.id = id;}
+
+    public UUID getRestaurantId() {return restaurantId;}
+
+    public void setRestaurantId(UUID restaurantId) {this.restaurantId = restaurantId;}
+
+    public String getComment() {return comment;}
+
+    public void setComment(String comment) {this.comment = comment;}
+}
