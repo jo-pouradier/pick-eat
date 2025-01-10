f3acddc6d1417556426942c2fae0a9829167b0b0
Adrien <adrien.dalbeigue@cpe.fr>
2025-01-10 16:39:23 +0100

    debugging

--- restaurant/src/main/java/fr/pick_eat/restaurant/repository/RestaurantNoteRepository.java
+++ restaurant/src/main/java/fr/pick_eat/restaurant/repository/RestaurantNoteRepository.java
@@ -2,8 +2,10 @@ package fr.pick_eat.restaurant.repository;
 
 import fr.pick_eat.restaurant.entity.RestaurantNoteModel;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface RestaurantNoteRepository extends CrudRepository<RestaurantNoteModel, UUID> {}
 
--- restaurant/src/main/java/fr/pick_eat/restaurant/repository/RestaurantRepository.java
+++ restaurant/src/main/java/fr/pick_eat/restaurant/repository/RestaurantRepository.java
@@ -1,15 +1,20 @@
 package fr.pick_eat.restaurant.repository;
 
-import fr.pick_eat.restaurant.dto.RestaurantDTO;
 import fr.pick_eat.restaurant.entity.RestaurantModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface RestaurantRepository extends CrudRepository<RestaurantModel, UUID> {
 
-    public List<RestaurantDTO> findBetweenLatAndLon(double minLat, double maxLat, double minLon, double maxLon);
+
+
+//    @Query("SELECT r FROM RestaurantModel r WHERE r.lat BETWEEN :minLat AND :maxLat AND r.lon BETWEEN :minLon AND maxLon")
+//    public <RestaurantDTO> void findBetweenLatAndLon(double minLat, double maxLat, double minLon, double maxLon);
 
 }
 
