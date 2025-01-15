cf206586f3269829e0e03b3f5ae9c79b46232e96
jo-pouradier <joseph.pouradier-duteil@cpe.fr>
2025-01-15 16:38:07 +0100

    add swagger with jwt auth

---
+++ event/src/main/java/fr/pick_eat/event/config/SwaggerConfig.java
@@ -0,0 +1,32 @@
+package fr.pick_eat.event.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        final String securitySchemeName = "jwt";
+        return new OpenAPI()
+                .info(new Info().title("Event Service")
+                        .description("Documentation Event Service")
+                        .version("v0.0.1"))
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.APIKEY)
+                                                .in(SecurityScheme.In.COOKIE)
+                                                .bearerFormat("jwt")));
+    }
+}
---
+++ restaurant/src/main/java/fr/pick_eat/restaurant/controller/SwaggerController.java
@@ -0,0 +1,18 @@
+package fr.pick_eat.restaurant.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import io.swagger.v3.oas.annotations.Hidden;
+
+@Hidden
+@RestController
+public class SwaggerController {
+    
+    @GetMapping("/")
+    public RedirectView index() {
+        // redirect to swagger-ui.html
+        return new RedirectView("/swagger-ui/index.html");
+    }
+}
